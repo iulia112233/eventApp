383a0e09cb16797e7277447c14457f652f436608
iulia112233 <34300511+iulia112233@users.noreply.github.com>
2018-02-14 09:51:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = './static/img/'
 GALLERY_FOLDER = './static/gallery/'
 DEFAULT_EVENT_IMAGE = 'events-default.jpg'
-MAIL_SERVER = 'email-smtp.eu-west-1.amazonaws.com'
-MAIL_PORT = '587'
-MAIL_USERNAME = 'AKIAIF5PHUECDHRYQ4QA'
-MAIL_PASSWORD = 'AqCaqA93CQTI2L9kyYKjgTl5xtx9Jd3kfi3tMxQJf0XY'
+MAIL_SERVER = ''
+MAIL_PORT = ''
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
 MAIL_USE_TLS = False
 MAIL_USE_SSL = False
-ADMINS = ['iulia@phyramid.com']
-STRIPE_PK = 'pk_test_c9A05XZrxznbqyFQz3iVSm9R'
-STRIPE_SK = 'sk_test_GeyyUpAGfkv3yAGSIR7hQQPQ'
+ADMINS = []
+STRIPE_PK = ''
+STRIPE_SK = ''
 
  
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@localhost:3306/bd'
